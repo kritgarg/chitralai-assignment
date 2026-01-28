c5709e3a587de355c6cdc263bdda39b2cf688dba
kritgarg <kritg0160@gmail.com>
2026-01-28 11:30:00 +0530

    Setup Zustand state management

---
+++ src/store/themeStore.js
@@ -0,0 +1,7 @@
+import { create } from 'zustand';
+
+export const useThemeStore = create((set) => ({
+  isDarkMode: false,
+  toggleTheme: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
+  setDarkMode: (value) => set({ isDarkMode: value }),
+}));
